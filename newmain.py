35ba6400894a5ceba33526f33aed80633d1f8a3b
Dustin <stobbe.dustin@gmail.com>
2022-05-14 22:26:20 +0200

    added timer and entered and left link, as well as pickle file

--- newmain.py
+++ newmain.py
@@ -1,12 +1,25 @@
+from timeit import default_timer as timer
+import pickle
+
 from config import dbpath
 from db import *
 
+q_start=timer()
+
 db = Db(dbpath)
 vehicles = db.get_vehicles()
-getlinks = db.get_vehicle_links(vehicles)
-res = getlinks.get("drt406")
-print(get_link_from_id(res,"1"))
+enteredlinks = db.get_vehicle_entered_links(vehicles)
+leftlinks = db.get_vehicle_left_links(vehicles)
 
+with open('enteredlinks.pickle', 'wb') as fp:
+    pickle.dump(enteredlinks, fp)
+
+with open('leftlinks.pickle', 'wb') as fp:
+    pickle.dump(leftlinks, fp)
+
+q_end=timer()
+q_duration=q_end-q_start
+print('\n Total elapsed time: ', q_duration,'\n')
 
 def get_link_from_id(trips,id):
     for trip in trips:
