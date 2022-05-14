26fddc12714f90f50cfd37e43d3cdee684388f37
Dustin <stobbe.dustin@gmail.com>
2022-05-14 23:08:37 +0200

    added length and freespeed to class

--- vtypes.py
+++ vtypes.py
@@ -4,12 +4,15 @@ class Trip:
     link: str
     event_id: int
     time: int
+    link_length: float
+    link_freesped: float
 
 @dataclass
 class Vehicle:
     id: str
     trips: list[Trip]
 
+# atm unnecessary
 @dataclass
 class Links:
     link: str
