e02f36af0c3a591c00285dcb767f20651543decf
Dustin <stobbe.dustin@gmail.com>
2022-05-15 12:46:59 +0200

    added entered and left time for Trip, added attributes to Vehicle Class

--- vtypes.py
+++ vtypes.py
@@ -3,14 +3,20 @@ from dataclasses import dataclass
 class Trip:
     link: str
     event_id: int
-    time: int
+    entered_time: int
     link_length: float
-    link_freesped: float
+    link_freespeed: float
+    left_time: int = -1
 
 @dataclass
 class Vehicle:
     id: str
-    trips: list[Trip]
+    traveleddistance: float
+    intown_pct: float
+    countryroad_pct: float
+    highway_pct: float
+    avgpassenger_amount: float = -1
+    # trips: list[Trip]
 
 # atm unnecessary
 @dataclass
